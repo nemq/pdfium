f21921feeba02fead8cb29585eb2df47bff97d5a
Even Rouault <even.rouault@spatialys.com>
2015-09-08 17:54:04 +0200

    Various fixes to build-win.bat

--- build-win.bat
+++ build-win.bat
@@ -6,27 +6,33 @@ mkdir pdfium_deps
 cd pdfium_deps
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 git clone https://chromium.googlesource.com/external/gyp.git
-set PATH=c:\src\pdfium_deps\depot_tools;%PATH%
+set PATH=%BASE_DIR%\pdfium_deps\depot_tools;%PATH%
 
 cd gyp
 python setup.py install
 
 :PDFIUM
 cd %BASE_DIR%
-git clone https://github.com/rouault/pdfium win_gdal_build
+git clone https://github.com/rouault/pdfium
+cd pdfium
+git checkout win_gdal_build
+cd ..
 
 :BUILD
-cd %BASE_DIR%\pdfium
-
-python build\gyp_pdfium.py
+xcopy /E /I pdfium pdfium-x64
+ren pdfium pdfium-x86
 
 REM Building x64
-echo call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64 > build_x64.bat
-echo msbuild build\all.sln /p:Configuration=Release /p:Platform=x64 >> build_x64.bat
+cd %BASE_DIR%\pdfium-x64
+python build\gyp_pdfium.py
+echo call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64 > build_x64.bat
+echo msbuild build\all.sln /p:Configuration=Release /p:Platform=x64 /m >> build_x64.bat
 cmd /c build_x64.bat
 
 REM Building x86
+cd %BASE_DIR%\pdfium-x86
+python build\gyp_pdfium.py
 echo call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86 > build_x86.bat
-echo msbuild build\all.sln /p:Configuration=Release /p:Platform=Win32 >> build_x86.bat
+echo msbuild build\all.sln /p:Configuration=Release /p:Platform=Win32 /m >> build_x86.bat
 cmd /c build_x86.bat
 
