e6a9163bfeb4410b5a2b14074dac2b44dbd114c3
nemq <sawka.tomek@gmail.com>
2016-07-26 00:19:25 +0200

    fix unspecified \MACHINE flag error for x86 build

--- build-win.bat
+++ build-win.bat
@@ -26,13 +26,13 @@ REM Building x64
 cd %BASE_DIR%\pdfium-x64
 python build\gyp_pdfium.py
 echo call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64 > build_x64.bat
-echo msbuild build\all.sln /p:Configuration=Release /p:Platform=x64 /m >> build_x64.bat
+echo msbuild build\all.sln /p:Configuration=Release /p:Platform=x64 /p:PreferredToolArchitecture=x64 /m >> build_x64.bat
 cmd /c build_x64.bat
 
 REM Building x86
 cd %BASE_DIR%\pdfium-x86
 python build\gyp_pdfium.py
 echo call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86 > build_x86.bat
-echo msbuild build\all.sln /p:Configuration=Release /p:Platform=Win32 /m >> build_x86.bat
+echo msbuild build\all.sln /p:Configuration=Release /p:Platform=Win32 /p:PreferredToolArchitecture=x86 /m >> build_x86.bat
 cmd /c build_x86.bat
 
